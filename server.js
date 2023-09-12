f0112775d5ce00217ae513af13efdce272353fb5
lukaku999 <999lukaku@gmail.com>
2023-09-12 15:39:12 +0200

    first

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+// Define a route that responds with "Hello, World!"
+app.get("/calculate", (req, res) => {
+  const a = req.query.a;
+  const b = req.query.b;
+  const c = (a + b).toString();
+  res.send(`calculte: ${c}`);
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
